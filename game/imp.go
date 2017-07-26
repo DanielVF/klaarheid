9dd3801c71c8d93ec098a726abfa1f573d3cc8a6
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-07-26 13:55:41 +0100

    refactor

--- game/imp.go
+++ game/imp.go
@@ -1,7 +1,5 @@
 package game
 
-import "time"
-
 type Imp struct {
 	Mob
 }
@@ -28,43 +26,11 @@ func NewImp(w *World, x, y int, faction string) *Imp {
 
 func (i *Imp) AI() {
 
-	w := i.World
-
-	target := w.NearestPC(i.X, i.Y)
+	target := i.World.NearestPC(i.X, i.Y)
 
 	if target == nil {
 		return
 	}
 
-	distance_map := w.DistanceMap(target.GetX(), target.GetY())
-
-	max_moves := i.MovesLeft
-
-	for n := 0; n < max_moves; n++ {
-
-		best_dx := 0
-		best_dy := 0
-		best_tar_dist := NO_PATH
-
-		for _, neigh := range w.Neighbours(i.X, i.Y) {
-			if distance_map[neigh.X][neigh.Y] < best_tar_dist {
-				best_dx = neigh.X - i.X
-				best_dy = neigh.Y - i.Y
-				best_tar_dist = distance_map[neigh.X][neigh.Y]
-			}
-		}
-
-		if best_dx != 0 || best_dy != 0 {
-
-			success := i.TryMove(best_dx, best_dy)
-			w.Draw()
-			time.Sleep(50 * time.Millisecond)
-
-			// It's odd for this not to succeed, but...
-
-			if success == false {
-				return
-			}
-		}
-	}
+	i.PathTowards(target.GetX(), target.GetY())
 }
--- game/mob.go
+++ game/mob.go
@@ -1,10 +1,14 @@
 package game
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Mobber interface {
 	Reset()
-	TryMove(x, y int) bool
+	TryMove(x, y int)			bool
+	PathTowards(x, y int)
 	Key(key string)
 	AI()
 }
@@ -14,10 +18,10 @@ type Mobber interface {
 
 type Mob struct {
 	Thing
-	MovesLeft			int
-	ActionsLeft			int
-	Moves				int
-	Actions				int
+	MovesLeft					int
+	ActionsLeft					int
+	Moves						int
+	Actions						int
 }
 
 func (s *Mob) SelectionString() string {
@@ -46,6 +50,41 @@ func (s *Mob) TryMove(x, y int) bool {
 	}
 
 	return success
+}
+
+func (m *Mob) PathTowards(x, y int) {
+
+	w := m.World
+
+	distance_map := w.DistanceMap(x, y)
+
+	max_moves := m.MovesLeft
+
+	for n := 0; n < max_moves; n++ {
+
+		best_dx := 0
+		best_dy := 0
+		best_tar_dist := NO_PATH
+
+		for _, neigh := range w.Neighbours(m.X, m.Y) {
+			if distance_map[neigh.X][neigh.Y] < best_tar_dist {
+				best_dx = neigh.X - m.X
+				best_dy = neigh.Y - m.Y
+				best_tar_dist = distance_map[neigh.X][neigh.Y]
+			}
+		}
+
+		if best_dx != 0 || best_dy != 0 {
+
+			success := m.TryMove(best_dx, best_dy)
+			w.Draw()
+			time.Sleep(50 * time.Millisecond)
+
+			if success == false {
+				return
+			}
+		}
+	}
 }
 
 func (s *Mob) Key(key string) {
